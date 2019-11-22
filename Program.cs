b5e81d33f6ffc611f9a85aea74acf9551ffee0a2
TristanCho <51831659+TristanCho@users.noreply.github.com>
2019-11-22 21:02:03 +0100

    Pendiente por verificar que entre en un bucle de selección entre cifrar, decifrar o salir...

--- Program.cs
+++ Program.cs
@@ -10,25 +10,45 @@ namespace CodigoCesar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("realizar un programa que encripte un texto mediante el cifrado César. El usuario introducirá el texto, y el número de desplazamiento.");
+            Console.WriteLine("realizar un programa que encripte un texto mediante el cifrado César. \n" +
+                "El usuario introducirá el texto, y el número de desplazamiento.");
             Console.WriteLine("\n\n\n\n");
             String texto;
-          
-            Console.WriteLine("Introduce el mensaje a cifrar");
+            int ORDEN = 0;
+            uint desplazamiento;
+            char[] Caracteres;
+            int longitud;
+            
+
+            do
+            {
+                //CIFRAR
+                Console.WriteLine("Introduce el mensaje a cifrar");
+                texto = Console.ReadLine();
+                texto = texto.ToUpper();
+                Console.WriteLine("Introduce el número de desplazamiento");
+                desplazamiento = Convert.ToUInt32(Console.ReadLine());
+                Caracteres = texto.ToCharArray();
+
+                longitud = Caracteres.Length;
+                Console.WriteLine("Longitud= " + longitud);
+                CifrarMensaje(desplazamiento, texto, longitud);
+                Console.WriteLine("PARA DECIFRAR UN MENSAJE PULSE 0 ---- PARA CIFRAR OTRO MENSAJE PULSE 1");
+                ORDEN = Convert.ToInt32(Console.ReadLine());
+            } while (ORDEN == 1);
+
+            //DECIFRAR
+            Console.WriteLine("Introduce el mensaje a Decifrar");
             texto = Console.ReadLine();
             texto = texto.ToUpper();
             Console.WriteLine("Introduce el número de desplazamiento");
-            uint desplazamiento = Convert.ToUInt32(Console.ReadLine());
-            char[] Caracteres = texto.ToCharArray();
-            /*
-            bool comprobacion = Caracteres[].isLetterOrDigit();
-            if (comprobacion)
-            {
+            desplazamiento = Convert.ToUInt32(Console.ReadLine());
+            Caracteres = texto.ToCharArray();
 
-            }*/
-            int longitud = Caracteres.Length;
+            longitud = Caracteres.Length;
             Console.WriteLine("Longitud= " + longitud);
-            CifrarMensaje(desplazamiento, texto, longitud);
+            DeCifrarMensaje(desplazamiento, texto, longitud);
+           
 
             Console.ReadKey();
         }
@@ -39,8 +59,9 @@ namespace CodigoCesar
             char[] mochila = new char[longitud];
             for (int i = 0; i < longitud; i++)
             {
-                
                 uint valorX = clave + mensaje[i];
+                if (valorX > 'A' && valorX < 'Z') {
+                    
                 if (valorX>'Z')
                 {
                     valorX -= 26;
@@ -49,8 +70,41 @@ namespace CodigoCesar
                 {
                     valorX += 26;
                 }
+                    //else
+                      //  Console.WriteLine("!Letra=" + valorX);
+                }
                 char ValorConvertido = Convert.ToChar(valorX);
                 mochila[i]= ValorConvertido;
+                //pendiente de revisar los espacios... permitir ingresar sólo las letras if alpha que haga bien los espacios, verificar que luego se pueda desencriptar también
+                //No tardes mucho que hay que hacer las pacs ..
+            }
+            Console.WriteLine(mochila);
+        }
+
+        public static void DeCifrarMensaje(uint clave, string mensaje, int longitud)
+        {
+
+            char[] mochila = new char[longitud];
+            for (int i = 0; i < longitud; i++)
+            {
+                
+                uint valorX = mensaje[i] -clave;
+                if (valorX > 'A' && valorX < 'Z')
+                {
+                    if (valorX > 'Z')
+                    {
+                        valorX -= 26;
+                    }
+                    if (valorX < 'A')
+                    {
+                        valorX += 26;
+                    }
+                }
+                //else
+                  //  Console.WriteLine("!Letra="+valorX);
+                
+                char ValorConvertido = Convert.ToChar(valorX);
+                mochila[i] = ValorConvertido;
                 //pendiente de revisar los espacios... permitir ingresar sólo las letras if alpha que haga bien los espacios, verificar que luego se pueda desencriptar también
                 //No tardes mucho que hay que hacer las pacs ..
             }
